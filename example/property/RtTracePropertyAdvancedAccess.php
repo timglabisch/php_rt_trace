d9047bff13ed30ed9eb5785a61992f0e0292823c
timglabisch <tim@ainfach.de>
2022-02-12 23:52:39 +0100

    add todos

--- example/property/RtTracePropertyAdvancedAccess.php
+++ example/property/RtTracePropertyAdvancedAccess.php
@@ -17,16 +17,17 @@ class RtTracePropertyAdvancedAccess
         --$this->a;
         $this->a--;
         $this->a += 1;
-        /*
         $this->a -= 1;
         $this->a *= 1;
         $this->b['a']['b'];
+        $this->b['a']['b']++;
         shuffle($this->b);
         shuffle($this->b[0]);
-        shuffle($this->b + [a]);
+        foo($this->b + ['a']); // todo we could trace this the easy way
         $this->a ??= 1;
         $a = $this->a ? 1 : 2;
         $this->{'a'} = 1;
-        */
+        // todo, is this fine with uninitialized properties?
+        $x = $this->a ?? $this->b ?? $this->c ?? null;
     }
 }
--- example/property/RtTracePropertyAdvancedAccess.php.pretty.php
+++ example/property/RtTracePropertyAdvancedAccess.php.pretty.php
@@ -2,24 +2,41 @@
 
 namespace example\property;
 
-define('__RTc8d2065678cdd5dec24b0ff5ab3fa72b', array('RTc8d2065678cdd5dec24b0ff5ab3fa72b', '{"opcode":6,"id":"RTc8d2065678cdd5dec24b0ff5ab3fa72b","filename":"\\/Users\\/timglabisch\\/proj\\/php\\/php_rt_trace\\/tests\\/..\\/example\\/property\\/RtTracePropertyAdvancedAccess.php","hash":"b3598072ac0aa8bc2e1a38b6d88c75fe"}'));
+define('__RTc8d2065678cdd5dec24b0ff5ab3fa72b', array('RTc8d2065678cdd5dec24b0ff5ab3fa72b', '{"opcode":6,"id":"RTc8d2065678cdd5dec24b0ff5ab3fa72b","filename":"\\/Users\\/timglabisch\\/proj\\/php\\/php_rt_trace\\/tests\\/..\\/example\\/property\\/RtTracePropertyAdvancedAccess.php","hash":"e90f1ff8bd545711ee0e40078462a9d5"}'));
 class RtTracePropertyAdvancedAccess
 {
     private $a = 1;
     private $b = [];
     private function foo()
     {
-        \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->a += 1, __CLASS__, 'a', 11, 11, __RTc8d2065678cdd5dec24b0ff5ab3fa72b);
-        /*
-        $this->a -= 1;
-        $this->a *= 1;
-        $this->b['a']['b'];
-        shuffle($this->b);
+        \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->a++, __CLASS__, 'a', 11, 11, __RTc8d2065678cdd5dec24b0ff5ab3fa72b);
+        $this->a = \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyAssign(\timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->a++, __CLASS__, 'a', 12, 12, __RTc8d2065678cdd5dec24b0ff5ab3fa72b), __CLASS__, 'a', 12, 12, __RTc8d2065678cdd5dec24b0ff5ab3fa72b);
+        $this->a = \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyAssign($this->a = \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyAssign(\timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->a++, __CLASS__, 'a', 13, 13, __RTc8d2065678cdd5dec24b0ff5ab3fa72b), __CLASS__, 'a', 13, 13, __RTc8d2065678cdd5dec24b0ff5ab3fa72b), __CLASS__, 'a', 13, 13, __RTc8d2065678cdd5dec24b0ff5ab3fa72b);
+        // foo could not get a reference to $this->a because $this->a++ is a statement that you cant pass as a reference.
+        foo(\timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->a++, __CLASS__, 'a', 15, 15, __RTc8d2065678cdd5dec24b0ff5ab3fa72b));
+        \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch(++$this->a, __CLASS__, 'a', 16, 16, __RTc8d2065678cdd5dec24b0ff5ab3fa72b);
+        \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch(--$this->a, __CLASS__, 'a', 17, 17, __RTc8d2065678cdd5dec24b0ff5ab3fa72b);
+        \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->a--, __CLASS__, 'a', 18, 18, __RTc8d2065678cdd5dec24b0ff5ab3fa72b);
+        \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->a += 1, __CLASS__, 'a', 19, 19, __RTc8d2065678cdd5dec24b0ff5ab3fa72b);
+        \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->a -= 1, __CLASS__, 'a', 20, 20, __RTc8d2065678cdd5dec24b0ff5ab3fa72b);
+        \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->a *= 1, __CLASS__, 'a', 21, 21, __RTc8d2065678cdd5dec24b0ff5ab3fa72b);
+        \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->b, __CLASS__, 'b', 22, 22, __RTc8d2065678cdd5dec24b0ff5ab3fa72b)['a']['b'];
+        $this->b['a']['b']++;
+        (fn() => array(shuffle($this->b), \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->b, __CLASS__, 'b', 24, 24, __RTc8d2065678cdd5dec24b0ff5ab3fa72b)))()[0];
         shuffle($this->b[0]);
-        shuffle($this->b + [a]);
-        $this->a ??= 1;
-        $a = $this->a ? 1 : 2;
+        foo($this->b + ['a']);
+        // todo we could trace this the easy way
+        \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch(
+            // todo we could trace this the easy way
+            $this->a ??= 1,
+            __CLASS__,
+            'a',
+            27,
+            27,
+            __RTc8d2065678cdd5dec24b0ff5ab3fa72b
+        );
+        $a = \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->a, __CLASS__, 'a', 28, 28, __RTc8d2065678cdd5dec24b0ff5ab3fa72b) ? 1 : 2;
         $this->{'a'} = 1;
-        */
+        $x = \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->a, __CLASS__, 'a', 30, 30, __RTc8d2065678cdd5dec24b0ff5ab3fa72b) ?? \timglabisch\PhpRtTrace\RtInternalTracer::tracePropertyFetch($this->b, __CLASS__, 'b', 30, 30, __RTc8d2065678cdd5dec24b0ff5ab3fa72b) ?? $this->c ?? null;
     }
 }
